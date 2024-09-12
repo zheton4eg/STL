04f87409ab5e1c50167738eb40e8556836f0f39e
zheka <zheka@HOME-PC>
2024-09-12 21:21:07 +0300

    sequence contaners DONE

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,7 +13,8 @@ using namespace std;
 /////////////////////////////////////////////////////////
 
 //#define STL_ARRAY
-#define STL_VECTOR
+//#define STL_VECTOR
+#define STL_LIST
 template<typename T>void vector_info(const std::vector<T>& vec);
 void main()
 {
@@ -48,7 +50,57 @@ void main()
 	cout << endl;
 	vector_info(vec);
 	vec.reserve(15);
+
+	for (int i : vec)cout << i << tab; cout << endl;
+	vector_info(vec);
+
+	int index;
+	int value;
+	int count;
+
+	vec.insert(vec.begin() + 4, { 1024,2048,3072,4096 });
+	for (int i : vec)cout << i << tab; cout << endl;
+	cout << "¬ведите индекс добавл€емого элемента: "; cin >> index;
+	cout << "¬ведите кол-во добавл€емых элементов: "; cin >> count;
+	cout << "¬ведите значение добавл€емого элемента: "; cin >> value;
+
+
+	vec.insert(vec.begin()+index,count, value);
+	for (int i : vec)cout << i << tab; cout<<endl;
+
+	cout << "¬ведите индекс удал€емого элемента: "; cin >> index;
+	cout << "¬ведите кол-во удал€емого элементов: "; cin >> count;
+
+	vec.erase(vec.begin() + index, vec.begin()+index+count);
+	for (int i : vec)cout << i << tab; cout << endl;
+	
 #endif // STL_VECTOR
+#ifdef STL_LIST
+
+	std::list<int> list = { 3,5,8,13,21 };
+	for (std::list<int>::iterator it = list.begin();it!=list.end();++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	cout << list.front() << endl;
+	cout << list.back() << endl;
+	list.push_front(2);
+	list.push_back(34);
+	list.emplace_front(1);
+	for (int i : list)cout << i << tab; cout << endl;
+
+	int index;
+	int value;
+	cout << "¬ведите индекс добавл€емого элемента: "; cin >> index;
+	cout << "¬ведите значение добавл€емого элементов: "; cin >> value;
+	std::list<int>::iterator position = list.begin();
+	//for (int i = 0; i < index; i++)position++;
+	std::advance(position, index);
+	list.insert(position, value);
+	for (int i : list)cout << i << tab; cout << endl;
+
+#endif // STL_LIST
 }
 template<typename T>void vector_info(const std::vector<T>& vec)
 {
